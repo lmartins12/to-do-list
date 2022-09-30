4ccf92c159588989b1408febd7145dc83aa7a083
lmartins12 <lucas.martins559@outlook.com>
2022-09-30 03:47:43 -0300

    Updates

---
+++ precache-manifest.3a38eb568604fde69d6584a44d49cc9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0920a37a9a78e08c75d9",
+    "url": "/to-do-list/static/css/main.ef92e106.chunk.css"
+  },
+  {
+    "revision": "0920a37a9a78e08c75d9",
+    "url": "/to-do-list/static/js/main.6b5840d4.chunk.js"
+  },
+  {
+    "revision": "15bb3101340a9b514097",
+    "url": "/to-do-list/static/js/runtime~main.9900d293.js"
+  },
+  {
+    "revision": "3be6e82dfe4b0daf3e16",
+    "url": "/to-do-list/static/js/2.2561be16.chunk.js"
+  },
+  {
+    "revision": "5d7429a06ddad36be09150a4a24842cd",
+    "url": "/to-do-list/index.html"
+  }
+];
